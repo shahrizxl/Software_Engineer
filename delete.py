74d5c72c445b9ef5053d976495107f8c8f297623
shahrizxl <shahrizxl@gmail.com>
2024-11-08 21:12:06 +0800

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+# Connect to the database
+c = sqlite3.connect('database.sqlite3')
+
+# Correct DELETE query
+c.execute("DELETE FROM customer WHERE id = '040805100671'")
+
+# Commit the changes
+c.commit()
+
+# Close the connection
+c.close()
