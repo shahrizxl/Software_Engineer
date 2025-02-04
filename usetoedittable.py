d49fe1923b56b35c0a23fb55bcaac13a6c4161da
ami1212 <114460079+ami1212@users.noreply.github.com>
2025-02-05 00:13:45 +0800

    done htmlcss

--- usetoedittable.py
+++ usetoedittable.py
@@ -5,14 +5,8 @@ c = sqlite3.connect('database.sqlite3')
 cursor = c.cursor()
 
 cursor.execute("""
-    CREATE TABLE Feedback (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    ph TEXT NOT NULL,
-    content TEXT NOT NULL,
-    date DATE NOT NULL DEFAULT (date('now'))
-);
+    delete from Sales where type="Subtract"
+
 
 """)
 
